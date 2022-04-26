037cfb207d0613d41b8eee7dff1834b3b21b734b
MMPetrov20 <MMPetrov20@codingburgas.bg>
2022-04-26 23:30:48 +0300

    Add printing and console coloring algorithms. Close #7

--- source/Indigo/Indigo.cpp
+++ source/Indigo/Indigo.cpp
@@ -1,8 +1,73 @@
 #include <iostream>
-#include <stdlib.h> // for NULL
+#include <stdlib.h> // for NUll
+#include <Windows.h>
 
 using namespace std;
 
+void textcolor(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void printCardStack(bool array[13][7], int arrayRows, int arrayColumns)
+{
+    int cutoff = 5;
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns - cutoff; j++)
+        {
+            if (i >= 5 && i <= 6)
+            {
+                textcolor(1);
+                cout << array[i][j] << " ";
+            }
+            else
+            {
+                textcolor(15);
+
+                if (j == 0 && (i < 5 || i> 6))
+                {
+                    switch (i)
+                    {
+                    case 4:
+                    case 7:
+                        cout << " ";
+                        break;
+                    case 3:
+                    case 8:
+                        cout << "  ";
+                        break;
+                    case 2:
+                    case 9:
+                        cout << "   ";
+                        break;
+                    case 1:
+                    case 10:
+                        cout << "    ";
+                        break;
+                    case 0:
+                    case 11:
+                        cout << "     ";
+                        break;
+                    }
+                }
+                cout << array[i][j] << " ";
+            }
+        }
+        cout << endl;
+        if (i < 6)
+        {
+            if (i != 5)
+            {
+                cutoff--;
+            }
+        }
+        else
+        {
+            cutoff++;
+        }
+    }
+}
 int main()
 {
     bool cardStack[13][7];
@@ -33,4 +98,6 @@ int main()
             }
         }
     }
+
+    printCardStack(cardStack, cardStackRows, cardStackColumns);
 }
